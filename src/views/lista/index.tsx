e494be89a940d3a46d07bfbd681c48a799908b51
Camila Alves <alvescamila87@gmail.com>
2024-09-10 10:33:51 -0300

    create pessoas list component and bind to menu

---
+++ src/views/lista/index.tsx
@@ -0,0 +1,19 @@
+export default function ListaPessoas() {
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID Pessoa</th>
+                    <th>Nome completo</th>
+                    <th>CPF</th>
+                    <th>Telefone</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
